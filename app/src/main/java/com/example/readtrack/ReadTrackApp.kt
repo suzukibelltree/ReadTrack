3fa9e4b1cf64f618770469d818a2144c72a849ef
suzukibelltree <suzuki879455@gmail.com>
2024-11-05 11:59:30 +0900

    FABを追加

--- app/src/main/java/com/example/readtrack/ReadTrackApp.kt
+++ app/src/main/java/com/example/readtrack/ReadTrackApp.kt
@@ -1,14 +1,17 @@
 package com.example.readtrack
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -62,6 +66,18 @@ fun ReadTrackApp() {
                     Icon(Icons.Default.Settings, contentDescription = "Setting")
                 }
                 Spacer(modifier = Modifier.weight(1f))
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add"
+                )
             }
         }
     ) { innerPadding ->
