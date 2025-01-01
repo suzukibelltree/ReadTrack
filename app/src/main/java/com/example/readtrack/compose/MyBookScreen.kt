2281dc99e2445a8a415ad23bd2720762ec3cc900
suzukibelltree <suzuki879455@gmail.com>
2025-01-01 23:16:44 +0900

    MyBookScreenのUIを変更

--- app/src/main/java/com/example/readtrack/compose/MyBookScreen.kt
+++ app/src/main/java/com/example/readtrack/compose/MyBookScreen.kt
@@ -1,14 +1,22 @@
 package com.example.readtrack.compose
 
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -24,7 +32,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -32,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.readtrack.ReadTrackScreen
+import com.example.readtrack.network.BookData
 import com.example.readtrack.room.SavedBooksViewModel
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -48,7 +59,6 @@ fun MyBookScreen(
     savedBooksViewModel: SavedBooksViewModel,
     navController: NavController
 ) {
-    // TODO: 全体的に使用感が良くないのでUIの見直しを行う
     LaunchedEffect(bookId) {
         savedBooksViewModel.fetchBookDetails(bookId)
     }
@@ -56,21 +66,21 @@ fun MyBookScreen(
     val currentDateTime = LocalDateTime.now()
     val formattedDate = currentDateTime.format(DateTimeFormatter.ofPattern("yyyy/MM/dd/HH:mm"))
     selectedBook?.let { book ->
+        var selectedOption by remember {
+            mutableStateOf(
+                when (book.progress) {
+                    0 -> "未読"
+                    1 -> "読書中"
+                    else -> "読了"
+                }
+            )
+        }
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             var readPagesCount by remember { mutableStateOf(book.readpage) }
             var comment by remember { mutableStateOf(book.comment) }
-            var selectedOption by remember {
-                mutableStateOf(
-                    when (book.progress) {
-                        0 -> "未読"
-                        1 -> "読書中"
-                        else -> "読了"
-                    }
-                )
-            }
             val context = LocalContext.current
             Row {
                 AsyncImage(
@@ -103,21 +113,46 @@ fun MyBookScreen(
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                //状態の変更、読了ページ数の変更、感想の記入をここで行う
+                Text(
+                    text = "読書状態",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.Start)
+                )
+                //状態の変更、読了ページ数の変更、メモの記入をここで行う
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .align(Alignment.CenterHorizontally),
+                    horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
-                    Text(
-                        text = "読書状態",
-                        fontSize = 16.sp,
-                    )
-                    EditBookState(
-                        selectedOption = selectedOption,
-                        onSelectionChange = { newSelection ->
-                            selectedOption = newSelection
-                        }
-                    )
+                    ReadStateCard(
+                        book,
+                        0,
+                        com.example.readtrack.R.drawable.frame,
+                        "未読",
+                        onProgressChange = {
+                            selectedOption = "未読"
+                            book.progress = 0
+                        })
+                    ReadStateCard(
+                        book,
+                        1,
+                        com.example.readtrack.R.drawable.reading,
+                        "読書中",
+                        onProgressChange = {
+                            selectedOption = "読書中"
+                            book.progress = 1
+                        })
+                    ReadStateCard(
+                        book,
+                        2,
+                        com.example.readtrack.R.drawable.finished,
+                        "読了",
+                        onProgressChange = {
+                            selectedOption = "読了"
+                            book.progress = 2
+                        })
                 }
                 Row(
                     verticalAlignment = Alignment.CenterVertically
@@ -144,12 +179,16 @@ fun MyBookScreen(
                         modifier = Modifier.align(Alignment.CenterVertically)
                     )
                 }
-                Text("感想")
+                Text(
+                    text = "メモ",
+                    modifier = Modifier.align(Alignment.Start)
+                )
                 OutlinedTextField(
                     value = comment.toString(),
                     onValueChange = { comment = it },
                     modifier = Modifier.padding(8.dp)
                 )
+                //TODO: 保存する情報を感想ではなくメモにするか検討
                 Button(
                     onClick = {
                         Toast.makeText(context, "変更を保存しました", Toast.LENGTH_SHORT).show()
@@ -167,7 +206,10 @@ fun MyBookScreen(
                             )
                         )
                     },
-                    modifier = Modifier.padding(8.dp)
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp)
                 ) {
                     Text("変更を保存する")
                 }
@@ -179,7 +221,11 @@ fun MyBookScreen(
                         Toast.makeText(context, "ライブラリから削除しました", Toast.LENGTH_SHORT)
                             .show()
                     },
-                    modifier = Modifier.padding(8.dp)
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = ButtonDefaults.buttonColors(Color.Black)
                 ) {
                     Text("ライブラリから削除する")
                 }
@@ -229,6 +275,53 @@ fun EditBookState(
                     })
             }
         }
+    }
+}
+
+/**
+ * 本の読書状況(3状態)を表示するカード
+ * @param book 本の情報
+ * @param progress 本の読書状況
+ * @param icon アイコン
+ * @param contentDescription アイコンの説明
+ */
+@Composable
+fun ReadStateCard(
+    book: BookData,
+    progress: Int,
+    @DrawableRes icon: Int,
+    contentDescription: String,
+    onProgressChange: (Int) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .size(100.dp)
+            .clickable { onProgressChange(progress) }
+            .padding(8.dp)
+            .border(
+                width = 2.dp,
+                color = if (
+                    book.progress == progress
+                ) Color.Blue else Color.Gray,
+                shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .size(48.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = when (progress) {
+                0 -> "未読"
+                1 -> "読書中"
+                else -> "読了"
+            },
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
 
