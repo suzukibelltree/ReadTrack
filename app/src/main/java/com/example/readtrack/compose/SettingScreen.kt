1fba8fcf57943651f11242e1cce52a51c76cb19a
suzukibelltree <suzuki879455@gmail.com>
2025-03-08 21:33:41 +0900

    設定画面のUIを作成

--- app/src/main/java/com/example/readtrack/compose/SettingScreen.kt
+++ app/src/main/java/com/example/readtrack/compose/SettingScreen.kt
@@ -1,16 +1,31 @@
 package com.example.readtrack.compose
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -27,36 +42,173 @@ fun SettingScreen(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            text = stringResource(R.string.setting_character_size),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { }
-        )
+        Column {
+            var sizeMenuExpand by remember { mutableStateOf(false) }
+            Row {
+                Icon(
+                    imageVector = if (sizeMenuExpand) Icons.Default.Remove else Icons.Default.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { sizeMenuExpand = !sizeMenuExpand }
+                )
+                Text(
+                    text = stringResource(R.string.setting_character_size),
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+            AnimatedVisibility(sizeMenuExpand) {
+                val radiooptions1 = listOf("小", "中", "大")
+                val (selectedOption, onOptionSelected) = remember { mutableStateOf(radiooptions1[0]) }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp), // 選択肢の間にスペースを追加
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    radiooptions1.forEach { text ->
+                        Row(
+                            Modifier
+                                .selectable(
+                                    selected = (text == selectedOption),
+                                    onClick = { onOptionSelected(text) },
+                                    role = Role.RadioButton
+                                )
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = (text == selectedOption),
+                                onClick = null // null recommended for accessibility with screen readers
+                            )
+                            Text(
+                                text = text,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(start = 16.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
         HorizontalDivider()
-        Text(
-            text = stringResource(R.string.setting_theme_color),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { }
-        )
+        Column {
+            var themeMenuExpand by remember { mutableStateOf(false) }
+            Row {
+                Icon(
+                    imageVector = if (themeMenuExpand) Icons.Default.Remove else Icons.Default.Add,
+                    contentDescription = "Open Menu",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { themeMenuExpand = !themeMenuExpand }
+                )
+                Text(
+                    text = stringResource(R.string.setting_theme_color),
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+            AnimatedVisibility(themeMenuExpand) {
+                val radiooptions2 = listOf("青", "赤", "緑")
+                val (selectedOption, onOptionSelected) = remember { mutableStateOf(radiooptions2[0]) }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp), // 選択肢の間にスペースを追加
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    radiooptions2.forEach { text ->
+                        Row(
+                            Modifier
+                                .selectable(
+                                    selected = (text == selectedOption),
+                                    onClick = { onOptionSelected(text) },
+                                    role = Role.RadioButton
+                                )
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = (text == selectedOption),
+                                onClick = null // null recommended for accessibility with screen readers
+                            )
+                            Text(
+                                text = text,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(start = 16.dp)
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
         HorizontalDivider()
-        Text(
-            text = stringResource(R.string.setting_switch_dark_light),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .clickable { }
-        )
+        Column {
+            var switchDarkExpand by remember { mutableStateOf(false) }
+            Row {
+                Icon(
+                    imageVector = if (switchDarkExpand) Icons.Default.Remove else Icons.Default.Add,
+                    contentDescription = "Open Menu",
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { switchDarkExpand = !switchDarkExpand }
+                )
+                Text(
+                    text = stringResource(R.string.setting_switch_dark_light),
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+            AnimatedVisibility(switchDarkExpand) {
+                val radiooptions3 = listOf("ダーク", "ライト")
+                val (selectedOption, onOptionSelected) = remember { mutableStateOf(radiooptions3[0]) }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp), // 選択肢の間にスペースを追加
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    radiooptions3.forEach { text ->
+                        Row(
+                            Modifier
+                                .selectable(
+                                    selected = (text == selectedOption),
+                                    onClick = { onOptionSelected(text) },
+                                    role = Role.RadioButton
+                                )
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = (text == selectedOption),
+                                onClick = null // null recommended for accessibility with screen readers
+                            )
+                            Text(
+                                text = text,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.padding(start = 16.dp)
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
         HorizontalDivider()
         Text(
             text = "ログアウト",
-            fontSize = 24.sp,
+            fontSize = 20.sp,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
