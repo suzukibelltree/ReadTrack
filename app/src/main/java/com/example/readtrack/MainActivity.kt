f3e130e09333a08970c3936b31c245b1aaab0921
suzukibelltree <suzuki879455@gmail.com>
2025-03-14 15:12:59 +0900

    初回起動時のみ説明ダイアログを表示するように変更した

--- app/src/main/java/com/example/readtrack/MainActivity.kt
+++ app/src/main/java/com/example/readtrack/MainActivity.kt
@@ -10,9 +10,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.readtrack.compose.HowToUseDialog
+import com.example.readtrack.datastore.isFirstLaunch
 import com.example.readtrack.ui.theme.ReadTrackTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,6 +41,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ReadTrackTheme {
+                var showDialog by remember { mutableStateOf(false) }
+
+                // 初回起動時にダイアログを表示
+                LaunchedEffect(Unit) {
+                    val isFirstLaunch = isFirstLaunch(this@MainActivity)
+                    if (isFirstLaunch) {
+                        showDialog = true
+                    }
+                }
+
+                if (showDialog) {
+                    HowToUseDialog(onDismiss = { showDialog = false })
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
