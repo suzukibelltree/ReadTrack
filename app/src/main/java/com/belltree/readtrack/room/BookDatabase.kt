87b8b2d1f4c844b18ec85f70018eec6233ea4b0a
suzukibelltree <suzuki879455@gmail.com>
2025-04-19 22:46:27 +0900

    うまく動作しないのでマイグレーション処理を削除

--- app/src/main/java/com/belltree/readtrack/room/BookDatabase.kt
+++ app/src/main/java/com/belltree/readtrack/room/BookDatabase.kt
@@ -4,11 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.belltree.readtrack.network.BookData
 
-@Database(entities = [BookData::class, ReadLog::class], version = 2)
+@Database(entities = [BookData::class, ReadLog::class], version = 1)
 abstract class BookDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
     abstract fun readLogDao(): ReadLogDao
@@ -22,35 +20,8 @@ abstract class BookDatabase : RoomDatabase() {
                 Room.databaseBuilder(
                     context, BookDatabase::class.java, "books_database"
                 )
-                    .addMigrations(Migration_1_2)
                     .build()
                     .also { INSTANCE = it }
-            }
-        }
-
-        private val Migration_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL(
-                    """
-                    CREATE TABLE ReadLog_new (
-                        logId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                        bookId TEXT NOT NULL,
-                        readPages INTEGER NOT NULL,
-                        recordedAt TEXT NOT NULL,
-                        yearMonthId INTEGER NOT NULL
-                    )
-                """.trimIndent()
-                )
-
-                database.execSQL(
-                    """
-                    INSERT INTO ReadLog_new (bookId, readPages, recordedAt, yearMonthId)
-                    SELECT 'unknown', readPages, '1970-01-01', yearMonthId FROM ReadLog
-                """.trimIndent()
-                )
-
-                database.execSQL("DROP TABLE ReadLog")
-                database.execSQL("ALTER TABLE ReadLog_new RENAME TO ReadLog")
             }
         }
     }
