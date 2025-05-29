27c171d1e7308c45aa51b127b2f80459933961a7
suzukibelltree <suzuki879455@gmail.com>
2025-05-29 16:24:13 +0900

    問題点を修正した新しいリリースを作成

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.belltree.readtrack"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0.13"
+        versionCode = 15
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
