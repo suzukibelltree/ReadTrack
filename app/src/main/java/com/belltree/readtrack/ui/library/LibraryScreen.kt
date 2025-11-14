fa94fa0cd7b26cd636a7e2fd952b07aa74146d27
suzukibelltree <suzuki879455@gmail.com>
2025-11-14 20:39:55 +0900

    LibraryScreenで左右にスワイプ移動できるように修正

--- app/src/main/java/com/belltree/readtrack/ui/library/LibraryScreen.kt
+++ app/src/main/java/com/belltree/readtrack/ui/library/LibraryScreen.kt
@@ -3,13 +3,17 @@ package com.belltree.readtrack.ui.library
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Tab
@@ -17,10 +21,7 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,6 +35,7 @@ import coil.compose.AsyncImage
 import com.belltree.readtrack.R
 import com.belltree.readtrack.themecolor.AppColors
 import com.belltree.readtrack.ui.navigation.Route
+import kotlinx.coroutines.launch
 
 /**
  * ライブラリ画面
@@ -49,17 +51,22 @@ fun LibraryScreen(
     val configuration = LocalConfiguration.current
     val screenWidthDp = configuration.screenWidthDp.dp
     val uiState = libraryViewModel.uiState.collectAsState()
-    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val pagerState = rememberPagerState(initialPage = 0) { 3 }
+    val scope = rememberCoroutineScope()
     val textColor = AppColors.textColor
     Column {
         TabRow(
-            selectedTabIndex = selectedTabIndex,
+            selectedTabIndex = pagerState.currentPage,
             contentColor = Color.Companion.Blue,
             modifier = Modifier.Companion.fillMaxWidth()
         ) {
             Tab(
-                selected = selectedTabIndex == 0,
-                onClick = { selectedTabIndex = 0 },
+                selected = pagerState.currentPage == 0,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(0)
+                    }
+                },
                 text = {
                     Text(
                         text = stringResource(R.string.read_state_unread),
@@ -68,8 +75,12 @@ fun LibraryScreen(
                 }
             )
             Tab(
-                selected = selectedTabIndex == 1,
-                onClick = { selectedTabIndex = 1 },
+                selected = pagerState.currentPage == 1,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(1)
+                    }
+                },
                 text = {
                     Text(
                         text = stringResource(R.string.read_state_reading),
@@ -78,8 +89,12 @@ fun LibraryScreen(
                 }
             )
             Tab(
-                selected = selectedTabIndex == 2,
-                onClick = { selectedTabIndex = 2 },
+                selected = pagerState.currentPage == 2,
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(2)
+                    }
+                },
                 text = {
                     Text(
                         text = stringResource(R.string.read_state_read),
@@ -103,56 +118,59 @@ fun LibraryScreen(
 
             is LibraryUiState.Success -> {
                 val savedBooks = state.bindingModel.libraryBookBindingModel
-                LazyVerticalGrid(columns = GridCells.Adaptive(screenWidthDp / 3)) {
-                    // ここでselectedTabIndexに応じて表示する本のリストを変える
-                    val filteredBooks = when (selectedTabIndex) {
-                        0 -> savedBooks.filter { it.progress == 0 }
-                        1 -> savedBooks.filter { it.progress == 1 }
-                        2 -> savedBooks.filter { it.progress == 2 }
-                        else -> savedBooks
-                    }
-                    items(filteredBooks) { book ->
-                        Column {
-                            if (book.thumbnail != null) {
-                                AsyncImage(
-                                    model = book.thumbnail,
-                                    contentDescription = null,
-                                    modifier = Modifier.Companion
-                                        .fillMaxWidth()
-                                        .height(120.dp)
-                                        .padding(16.dp)
-                                        .clickable {
-                                            navController.navigate("${Route.MyBook}/${book.id}")
-                                        }
-                                )
-                            } else {
-                                Image(
-                                    painter = painterResource(R.drawable.unknown),
-                                    contentDescription = "thumbnail not found",
-                                    modifier = Modifier.Companion
-                                        .fillMaxWidth()
-                                        .height(120.dp)
-                                        .padding(16.dp)
-                                        .clickable {
-                                            navController.navigate("${Route.MyBook}/${book.id}")
-                                        }
-                                )
-                            }
-                            Text(
-                                text = if (selectedTabIndex == 0) {
-                                    book.registeredDate.substring(0, 10)
-                                } else {
-                                    book.updatedDate.substring(0, 10)
-                                },
-                                modifier = Modifier.Companion.align(Alignment.Companion.CenterHorizontally)
-                            )
-                            if (book.pageCount != null && book.pageCount != 0) {
-                                LinearProgressIndicator(
-                                    progress = { book.readPages!!.toFloat() / book.pageCount.toFloat() },
-                                    modifier = Modifier.Companion
-                                        .fillMaxWidth()
-                                        .padding(8.dp),
-                                )
+                HorizontalPager(state = pagerState) { page ->
+                    Box(Modifier.fillMaxSize()) {
+                        val filteredBooks = when (pagerState.currentPage) {
+                            0 -> savedBooks.filter { it.progress == 0 }
+                            1 -> savedBooks.filter { it.progress == 1 }
+                            2 -> savedBooks.filter { it.progress == 2 }
+                            else -> savedBooks
+                        }
+                        LazyVerticalGrid(columns = GridCells.Adaptive(screenWidthDp / 3)) {
+                            items(filteredBooks) { book ->
+                                Column {
+                                    if (book.thumbnail != null) {
+                                        AsyncImage(
+                                            model = book.thumbnail,
+                                            contentDescription = null,
+                                            modifier = Modifier.Companion
+                                                .fillMaxWidth()
+                                                .height(120.dp)
+                                                .padding(16.dp)
+                                                .clickable {
+                                                    navController.navigate("${Route.MyBook}/${book.id}")
+                                                }
+                                        )
+                                    } else {
+                                        Image(
+                                            painter = painterResource(R.drawable.unknown),
+                                            contentDescription = "thumbnail not found",
+                                            modifier = Modifier.Companion
+                                                .fillMaxWidth()
+                                                .height(120.dp)
+                                                .padding(16.dp)
+                                                .clickable {
+                                                    navController.navigate("${Route.MyBook}/${book.id}")
+                                                }
+                                        )
+                                    }
+                                    Text(
+                                        text = if (pagerState.currentPage == 0) {
+                                            book.registeredDate.substring(0, 10)
+                                        } else {
+                                            book.updatedDate.substring(0, 10)
+                                        },
+                                        modifier = Modifier.Companion.align(Alignment.Companion.CenterHorizontally)
+                                    )
+                                    if (book.pageCount != null && book.pageCount != 0) {
+                                        LinearProgressIndicator(
+                                            progress = { book.readPages!!.toFloat() / book.pageCount.toFloat() },
+                                            modifier = Modifier.Companion
+                                                .fillMaxWidth()
+                                                .padding(8.dp),
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
