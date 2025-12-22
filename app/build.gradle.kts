f00ddece168cc524d23a2f15dd451f798b60e5fb
suzukibelltree <suzuki879455@gmail.com>
2025-12-23 07:43:25 +0900

    versionCodeを上げた

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.belltree.readtrack"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.0.20"
+        versionCode = 22
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
