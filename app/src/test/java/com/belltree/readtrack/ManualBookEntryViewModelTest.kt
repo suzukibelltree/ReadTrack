0fdd7431588a3f52a4f9a50ef884c0e22132aaf8
suzukibelltree <suzuki879455@gmail.com>
2026-05-13 22:46:35 +0900

    viewmodelのテスト不足ぶんを実装

--- app/src/test/java/com/belltree/readtrack/ManualBookEntryViewModelTest.kt
+++ app/src/test/java/com/belltree/readtrack/ManualBookEntryViewModelTest.kt
@@ -1,10 +1,16 @@
 package com.belltree.readtrack
 
+import app.cash.turbine.test
 import com.belltree.readtrack.domain.usecase.SaveManualBookUseCase
 import com.belltree.readtrack.ui.registermanually.ManualBookEntryViewModel
+import com.belltree.readtrack.ui.registermanually.ManualBookUiEvent
+import io.mockk.coVerify
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -39,5 +45,60 @@ class ManualBookEntryViewModelTest {
 
         val state = viewModel.formState.value
         assertEquals("JetBrains", state.author)
+    }
+
+    @Test
+    fun saveBook_withBlankTitle_doesNotCallUseCase() = runTest {
+        var onSavedCalled = false
+
+        viewModel.saveBook { onSavedCalled = true }
+        advanceUntilIdle()
+
+        coVerify(exactly = 0) { useCase(any(), any(), any(), any(), any(), any()) }
+        assertFalse(onSavedCalled)
+    }
+
+    @Test
+    fun saveBook_withValidTitle_callsUseCaseWithCorrectParams() = runTest {
+        viewModel.updateTitle("Kotlin入門")
+        viewModel.updateAuthor("著者A")
+        viewModel.updatePublisher("出版社B")
+        viewModel.updatePublishedDate("2024-01-01")
+        viewModel.updatePageCount("300")
+
+        viewModel.saveBook {}
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) {
+            useCase(
+                title = "Kotlin入門",
+                author = "著者A",
+                publisher = "出版社B",
+                publishedDate = "2024-01-01",
+                pageCount = "300",
+                thumbnail = null
+            )
+        }
+    }
+
+    @Test
+    fun saveBook_withValidTitle_emitsBookSavedEvent() = runTest {
+        viewModel.updateTitle("Kotlin入門")
+
+        viewModel.eventFlow.test {
+            viewModel.saveBook {}
+            assertEquals(ManualBookUiEvent.BookSaved, awaitItem())
+        }
+    }
+
+    @Test
+    fun saveBook_withValidTitle_callsOnSavedCallback() = runTest {
+        viewModel.updateTitle("Kotlin入門")
+        var onSavedCalled = false
+
+        viewModel.saveBook { onSavedCalled = true }
+        advanceUntilIdle()
+
+        assertTrue(onSavedCalled)
     }
 }
